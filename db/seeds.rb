a3c73e55cbe4ae7e6999e8f4de8ffd575945c078
sainingo <nickmentor421@gmail.com>
2022-08-16 21:23:41 +0300

    Database setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Greeting.create(message: "Entaa supa from samburu")
+Greeting.create(message: "Habari zenu from swahili")
+Greeting.create(message: "Hello from the English world")
+Greeting.create(message: "Asalem Alekum")
+Greeting.create(message: "Eserian pooki")
